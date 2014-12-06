de35027e7bb996f1451fbc25c85b7fa47b17388e
Alex Yakubovich <alex.yakubovich@gmail.com>
2014-12-06 16:40:58 -0500

    clean up presets

--- shiny_aks/federal_contributions_analysis/global.R
+++ shiny_aks/federal_contributions_analysis/global.R
@@ -1,4 +1,11 @@
 load('data/data')
+source('helper_functions/sort_by_count.R')
+make_na_last <- function(x) c(x[!is.na(x), 'N/A'])
+
+#presets
+province <- sort_by_count(data, 'province') %>% make_na_last
+city <- sort_by_count(data, 'city') %>% make_na_last
+target_riding <- sort_by_count(data, 'target_riding') %>% make_na_last
 
 nice_names <- list(party_name = 'Party Name',
                    province = 'Province',
@@ -7,8 +14,6 @@ nice_names <- list(party_name = 'Party Name',
                    target_riding = 'Riding'
                    )
 
-cohort_selection <- c('None', 
-                      'Party Name', 'Province', 'City', 'Federal Contribution', 'Riding')
+cohort_selection <- c('None', 'Party Name', 'Province', 'City', 'Federal Contribution', 'Riding')
 
 cohort_id <- c('coh1', 'coh2', 'coh3', 'coh4')
-
