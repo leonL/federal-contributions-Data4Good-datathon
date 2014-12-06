55ab7c53142a62f15fc205cab9041b634c3a3653
karolisjakaitis <karolis.jakaitis@gmail.com>
2014-12-06 11:35:53 -0500

    shiny app backbone

---
+++ shiny_aks/federal_contributions_analysis/ui.R
@@ -0,0 +1,34 @@
+shinyUI(fluidPage(
+  
+  fluidRow(
+    fluidRow(
+      column(12,
+             fluidRow(
+               column(8,
+                      h4('a8')
+                      ),
+               column(2,
+                      wellPanel(h4('a2'))                      
+                      ),
+               column(2,
+                      wellPanel(h4('a2'))
+                      )
+               
+               )
+             
+             
+             
+             )
+      
+      
+      
+      
+      )
+    
+    
+    )
+  
+  
+  
+  
+  ))
