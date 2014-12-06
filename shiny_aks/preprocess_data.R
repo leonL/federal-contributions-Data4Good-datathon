ddd6b472d0d276da598ae7163214f1f29ec206bf
Alex Yakubovich <alex.yakubovich@gmail.com>
2014-12-06 11:22:42 -0500

    preprocess data and write R data file

---
+++ shiny_aks/preprocess_data.R
@@ -0,0 +1,12 @@
+library(lubridate)
+library(dplyr)
+
+setwd("~/Projects/datathon/federal-contributions-analysis/shiny_aks/")
+data <- read.csv('../munged_data/all_contributions_2004_to_2013.csv')
+
+data <- data %>% mutate(date=contribution_date.adjusted, 
+                        date_month = month(contribution_date.adjusted),
+                        date_year = year(contribution_date.adjusted),
+                        date_year_month = paste(date_year, date_month, sep=""))
+
+data$contribution_amount_dollars <- data$contribution_amount/100
