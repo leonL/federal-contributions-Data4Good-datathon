0ee338bb16ad0aa3b42ea350926b143864ff9321
karolisjakaitis <karolis.jakaitis@gmail.com>
2014-12-06 12:15:45 -0500

    backbone ui

--- shiny_aks/federal_contributions_analysis/global.R
+++ shiny_aks/federal_contributions_analysis/global.R
@@ -1,2 +1,6 @@
 load('data/data')
 
+cohort_selection <- c('None', 
+                      'Party Name', 'Province', 'City', 'Federal Contribution', 'Riding')
+
+cohort_id <- c('coh1', 'coh2', 'coh3', 'coh4')
--- shiny_aks/federal_contributions_analysis/ui.R
+++ shiny_aks/federal_contributions_analysis/ui.R
@@ -6,29 +6,35 @@ shinyUI(fluidPage(
              fluidRow(
                column(8,
                       h4('a8')
-                      ),
+               ),
                column(2,
-                      wellPanel(h4('a2'))                      
-                      ),
+                      wellPanel(
+                        dateRangeInput(inputId = 'date', 
+                                       label = 'Select Date Range', 
+                                       start = min(data$date), 
+                                       end = max(data$date), 
+                                       min = min(data$date), 
+                                       max = max(data$date)),
+                        selectizeInput('party_name', 'Party Name', c('All', unique(data$party_name)), 'All', multiple=TRUE),
+                        selectizeInput('province', 'Province', c('All', unique(data$province)), 'All', multiple=TRUE),
+                        selectizeInput('city', 'City', c('All', unique(data$city)), 'All', multiple=TRUE),
+                        selectizeInput('flag.blank_contrib', 'Federal Contribution', c('All', unique(data$flag.blank_contrib)), 'All', multiple=TRUE),
+                        selectizeInput('target_riding', 'Riding', c('All', unique(data$target_riding)), 'All', multiple=TRUE)
+                        )                      
+               ),
                column(2,
-                      wellPanel(h4('a2'))
-                      )
-               
-               )
-             
-             
-             
-             )
-      
-      
-      
-      
+                      wellPanel(
+                        selectizeInput('date_coh', 'Choose Date Cohort', c('Year', 'Year-Month', 'Year-Month_Day'), multiple=FALSE),
+                        selectizeInput('coh1', 'Cohort 1', cohort_selection, multiple=FALSE),
+                        selectizeInput('coh2', 'Cohort 2', cohort_selection, multiple=FALSE),
+                        selectizeInput('coh3', 'Cohort 3', cohort_selection, multiple=FALSE),
+                        selectizeInput('coh3', 'Cohort 3', cohort_selection, multiple=FALSE)
+                        
+                        
+                        )
+               )               
+             )         
       )
-    
-    
     )
-  
-  
-  
-  
-  ))
+  )
+))
