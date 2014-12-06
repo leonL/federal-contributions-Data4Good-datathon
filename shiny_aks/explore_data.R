e747fbb47de4b718a5e6bda3398c1b695f63a5a2
karolisjakaitis <karolis.jakaitis@gmail.com>
2014-12-06 11:14:20 -0500

    super meaningful

--- shiny_aks/explore_data.R
+++ shiny_aks/explore_data.R
@@ -1,10 +1,10 @@
-
+library(data.table)
 library(lubridate)
-library(dplyr)
 library(ggplot2)
-setwd("~/Projects/datathon/federal-contributions-analysis")
-data <- raw_data <- read.csv('munged_data/all_contributions_2004_to_2013.csv')
+library(dplyr)
 
+data <- read.csv('../munged_data/all_contributions_2004_to_2013.csv')
+data <- data.table(data)
 
 data$date <- data$contribution_date.adjusted
 data$date_month <- month(data$contribution_date.adjusted)
@@ -12,6 +12,9 @@ data$date_year <- year(data$contribution_date.adjusted)
 data$date_year_month <- paste(data$date_year, data$date_month, sep="-")
 
 data$contribution_amount_dollars <- data$contribution_amount/100
+
+# There are some duplicates. Issue or multiple donations in the same day
+sum(duplicated(data))
 
 d <- data %>% 
   group_by(date, party_nam) %>% 
