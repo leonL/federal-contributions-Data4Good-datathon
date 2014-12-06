8bf6355d7012b3b2c681e1343fbca00b44668660
Alex Yakubovich <alex.yakubovich@gmail.com>
2014-12-06 17:01:48 -0500

    revert to old global while debugging

--- shiny_aks/federal_contributions_analysis/global.R
+++ shiny_aks/federal_contributions_analysis/global.R
@@ -2,13 +2,6 @@ library(googleVis)
 library(dplyr)
 
 load('data/data')
-source('helper_functions/sort_by_count.R')
-make_na_last <- function(x) c(x[!is.na(x), 'N/A'])
-
-#presets
-province <- sort_by_count(data, 'province') %>% make_na_last
-city <- sort_by_count(data, 'city') %>% make_na_last
-target_riding <- sort_by_count(data, 'target_riding') %>% make_na_last
 
 source('helper_functions/format_numbers.R')
 source('helper_functions/give_nice_names.R')
@@ -19,12 +12,15 @@ nice_names <- list(party_name = 'Party Name',
                    flag.blank_contrib = 'Federal Cont.',
                    target_riding = 'Riding',
                    n = 'Number of Contributions',
-                   avg_contribution = 'Average Cont.',
+                   avg_3contribution = 'Average Cont.',
                    total_contribution = 'Total Cont.',
                    date = 'Date',
                    date_year = 'Date-Year',
-                   date_year_month = 'Date-Month'
-                   )
+                   date_year_month = 'Date-Month',
+                   contributor_id = 'Contributor ID',
+                   full_name = 'Full Name',
+                   contribution_amount_dollars = 'Amount'
+)
 
 cohort_selection <- c('None', 
                       'Party Name', 'Province', 'City', 'Federal Cont.', 'Riding')
