712a64992dfb95e4c2c9a63e964c8be815ddeb73
Alex Yakubovich <alex.yakubovich@gmail.com>
2014-12-06 12:17:41 -0500

    seed nice names

--- shiny_aks/federal_contributions_analysis/global.R
+++ shiny_aks/federal_contributions_analysis/global.R
@@ -1,2 +1,8 @@
 load('data/data')
 
+nice_names <- list(party_name = 'Party Name',
+                   province = 'Province',
+                   city = 'City',
+                   federal_contribution = 'Federal Contribution',
+                   target_riding = 'Riding'
+                   )
